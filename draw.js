dd3e7accdd6e99ad4b72cd2447a5e71056b4ef12
LIUTaichen <liu_taichen@hotmail.com>
2017-11-17 12:06:58 +1300

    initial commit

---
+++ draw.js
@@ -0,0 +1,219 @@
+var mymap = L.map('mapid').setView([-36.914827, 174.8072903 ], 12);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: 'pk.eyJ1IjoiamFzb25saXV0YWljaGVuIiwiYSI6ImNqNmZ5ZGpkcjAzYWIzNXA1aWs5OXF3bXcifQ.33nis-JWUXYo1jpJkr1OSQ'
+}).addTo(mymap);
+
+
+
+
+//marker.bindPopup('<p>projects</p>');
+//marker.addTo(mymap);
+function loadFromJson(){
+    $.getJSON("projects.json", function(json) {
+        console.log(json); // this will show the info it in firebug console
+        
+        for(const project of json){
+            var marker = L.marker([project.Lat,project.Lng]);
+            var descriptionHtml = '';
+            descriptionHtml += '<div><p>' + project['Job ID and Title'] +'</p></div>';
+            descriptionHtml += '<p>' + project['Site Address'] +'</p></div>';
+            descriptionHtml += '<p>' + project['Contacts'] +'</p></div>';
+            descriptionHtml += '<div class="plants">';
+            //descriptionHtml += '<p>' + project['Booked'] +'</p></div>';
+            //descriptionHtml += '<p>' + project['Requires Booking'] +'</p></div>';
+            //descriptionHtml += '<p>' + project['Completed'] +'</p></div>';
+            if(project['Booked'].trim()){
+                descriptionHtml += '<h3>Booked Plants</h3>';
+                descriptionHtml += '<table class="plantTable blue"><tr><th>Plant #</th><th>Description</th><th>Device Needed</th>';
+                var bookedString = project['Booked'];
+                var bookedPlants = bookedString.split('\n');
+                for(const plant of bookedPlants){
+                    if(plant.includes('[')){
+                        let parts = plant.split('] [');
+                        for(let i = 0; i <=2; i++){
+                            parts[i] = parts[i].replace('[', '');
+                            parts[i] = parts[i].replace(']', '');
+                            parts[i] = parts[i].replace('Device type:', '');
+                            parts[i] = parts[i].trim();
+                        }
+                    
+                        descriptionHtml += '<tr><td>' +parts[0] +'</td><td>' + parts[1] + '</td><td>' + parts[2]+'</td></tr>'; 
+                    }
+                }
+            }
+            if(project['Requires Booking'].trim()){
+                descriptionHtml += '<h3>Plants requiring booking</h3>';
+                descriptionHtml += '<table class="plantTable pink"><tr><th>Plant #</th><th>Description</th><th>Device Needed</th>';
+                var requireString = project['Requires Booking'];
+                var requirePlants = requireString.split('\n');
+                for(const plant of requirePlants){
+                    if(plant.includes('[')){
+                        let parts = plant.split('] [');
+                        for(let i = 0; i <=2; i++){
+                            parts[i] = parts[i].replace('[', '');
+                            parts[i] = parts[i].replace(']', '');
+                            parts[i] = parts[i].replace('Device type:', '');
+                            parts[i] = parts[i].trim();
+                        }
+                    
+                        descriptionHtml += '<tr><td>' +parts[0] +'</td><td>' + parts[1] + '</td><td>' + parts[2]+'</td></tr>'; 
+                    }
+                }
+            }
+            if(project['Completed'].trim()){
+                descriptionHtml += '<h3>Plants completed</h3>';
+                descriptionHtml += '<table class="plantTable lightGreen"><tr><th>Plant #</th><th>Description</th><th>Device Needed</th>';
+                var completedString = project['Completed'];
+                var completedPlants = completedString.split('\n');
+                for(const plant of completedPlants){
+                    if(plant.includes('[')){
+                        let parts = plant.split('] [');
+                        for(let i = 0; i <=2; i++){
+                            parts[i] = parts[i].replace('[', '');
+                            parts[i] = parts[i].replace(']', '');
+                            parts[i] = parts[i].replace('Device type:', '');
+                            parts[i] = parts[i].trim();
+                        }
+                    
+                        descriptionHtml += '<tr><td>' +parts[0] +'</td><td>' + parts[1] + '</td><td>' + parts[2]+'</td></tr>'; 
+                    }
+                }
+            }
+            descriptionHtml += '</div>';
+            //console.log(bookedPlants);
+            marker.bindPopup(descriptionHtml);
+            marker.addTo(mymap);
+                
+        }
+    
+        
+    });
+}
+
+
+function processData(){
+   
+    console.log(plants);
+    console.log(projects);
+    var projectArray = {};
+    for(let project of projects){
+        //convert rows to array
+        projectArray[project['Job ID']] = project;
+    }
+
+    for(let plant of plants){
+        if(!plant['Department'] || !projectArray[plant['Department']]){
+            continue;
+        }
+        if(!plant['GPS'] && !plant['Excavator Kit'] && !plant['Oyster']){
+            continue;
+        }
+
+        let project = projectArray[plant['Department']];
+        if(plant['GPS Installation'] === 'Booked'){
+            if(!project['Booked'] ){
+                project['Booked'] = [];
+            }
+            project['Booked'].push(plant);
+        }else if(plant['GPS Installation'] === 'Completed'){
+            if(!project['Completed'] ){
+                project['Completed'] = [];
+            }
+            project['Completed'].push(plant);
+
+        }
+        else if(plant['GPS Installation'] === ''){
+            if(!project['Requires Booking'] ){
+                project['Requires Booking'] = [];
+            }
+            project['Requires Booking'].push(plant);
+        }
+    }
+
+    drawMarkers(projects);
+   
+}
+
+function convertRowToObject(data){
+    var headers = data.values[0];
+    console.log('headers');
+    console.log(headers);
+    var result = [];
+    for(let i = 1; i < data.values.length; i++){
+            var item = {};
+            for(let j = 0; j < headers.length; j ++){
+                item[headers[j]] = data.values[i][j]; 
+            }
+            result.push(item);
+    }
+    return result;
+}
+
+function drawMarkers(projects){
+    for(const project of projects){
+        let marker = L.marker([project.Lat,project.Lng]);
+        let descriptionHtml = preparePopupHtml(project);
+        marker.bindPopup(descriptionHtml);
+        marker.addTo(mymap);
+
+    }
+    
+}
+
+function preparePopupHtml(project){
+    let htmlString = '';
+    htmlString += '<div><p>' + project['Job ID and Title'] +'</p></div>';
+    htmlString += '<div><p>' + project['Site Address'] +'</p></div>';
+    htmlString += '<div><p> PM : ' + project['PM'] +'  ' +'<a href="mailto:'+project['PM email']+'">'+ project['PM email'] +'</a>  ' +project['PM contact']+'</p></div>';
+    htmlString += '<div><p> QS : ' + project['QS'] +'  ' +'<a href="mailto:'+project['QS email']+'">'+ project['QS email'] +'</a>  ' +project['QS contact']+'</p></div>';
+    htmlString += '<div><p> AM : ' + project['AM'] +'  ' +'<a href="mailto:'+project['AM email']+'">'+ project['AM email'] +'</a>  ' +project['AM contact']+'</p></div>';
+    htmlString += '<div class="plants">';
+    if(project['Booked']){
+        htmlString += '<h3>Booked Plants</h3>';
+        htmlString += '<table class="plantTable blue"><tr><th>Plant #</th><th>Description</th><th>Device Needed</th>';
+        for(let plant of project['Booked']){
+         
+            htmlString += convertPlantToTableRow(plant);
+        }
+        htmlString += '</table>';
+    }
+    if(project['Requires Booking']){
+        htmlString += '<h3>Plants requiring booking</h3>';
+        htmlString += '<table class="plantTable pink"><tr><th>Plant #</th><th>Description</th><th>Device Needed</th>';
+        for(let plant of project['Requires Booking']){
+               htmlString += convertPlantToTableRow(plant);
+           }
+        
+        htmlString += '</table>';
+    }
+    if(project['Completed']){
+        htmlString += '<h3>Plants completed</h3>';
+        htmlString += '<table class="plantTable lightGreen"><tr><th>Plant #</th><th>Description</th><th>Device Needed</th>';
+        for(let plant of project['Completed']){
+            htmlString += convertPlantToTableRow(plant);
+        }
+     
+        htmlString += '</table>';
+    }
+    htmlString+='</div>';
+    return htmlString;
+
+}
+
+function convertPlantToTableRow(plant){
+    let deviceString = '';
+    if(plant['GPS']){
+        deviceString += ' GPS';
+    }
+    if(plant['Excavator Kit']){
+        deviceString += ' Excavator Kit';
+    }
+    if(plant['Oyster']){
+        deviceString += ' Oyster';
+    }
+    return '<tr><td>' +plant['Plant #'] +'</td><td>' + plant['Description'] + '</td><td>' + deviceString+'</td></tr>'; 
+}
---
+++ google.js
@@ -0,0 +1,159 @@
+var CLIENT_ID = '676408930964-1lkbkp4ri2k3p3shv1daa71b7rvbvh6m.apps.googleusercontent.com';
+var API_KEY = 'AIzaSyDYeRC_Z2a9UrBLHaQ6yfgUilQZukYiYrA';
+
+// Array of API discovery doc URLs for APIs used by the quickstart
+var DISCOVERY_DOCS = ["https://sheets.googleapis.com/$discovery/rest?version=v4"];
+
+// Authorization scopes required by the API; multiple scopes can be
+// included, separated by spaces.
+var SCOPES = "https://www.googleapis.com/auth/spreadsheets.readonly";
+
+var authorizeButton = document.getElementById('authorize-button');
+var signoutButton = document.getElementById('signout-button');
+var plants;
+var projects;
+/**
+ *  On load, called to load the auth2 library and API client library.
+ */
+function handleClientLoad() {
+  gapi.load('client:auth2', initClient);
+}
+
+/**
+ *  Initializes the API client library and sets up sign-in state
+ *  listeners.
+ */
+function initClient() {
+  gapi.client.init({
+    apiKey: API_KEY,
+    clientId: CLIENT_ID,
+    discoveryDocs: DISCOVERY_DOCS,
+    scope: SCOPES
+  }).then(function () {
+    // Listen for sign-in state changes.
+    gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+
+    // Handle the initial sign-in state.
+    updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+    authorizeButton.onclick = handleAuthClick;
+    signoutButton.onclick = handleSignoutClick;
+  });
+}
+
+/**
+ *  Called when the signed in status changes, to update the UI
+ *  appropriately. After a sign-in, the API is called.
+ */
+function updateSigninStatus(isSignedIn) {
+  if (isSignedIn) {
+    authorizeButton.style.display = 'none';
+    signoutButton.style.display = 'block';
+    //listMajors();
+    var plants = getPlantList();
+    console.log(plants);
+  } else {
+    authorizeButton.style.display = 'block';
+    signoutButton.style.display = 'none';
+  }
+}
+
+/**
+ *  Sign in the user upon button click.
+ */
+function handleAuthClick(event) {
+  gapi.auth2.getAuthInstance().signIn();
+}
+
+/**
+ *  Sign out the user upon button click.
+ */
+function handleSignoutClick(event) {
+  gapi.auth2.getAuthInstance().signOut();
+}
+
+
+
+/**
+ * Print the names and majors of students in a sample spreadsheet:
+ * https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+ */
+
+ var plants;
+function listMajors() {
+    var result;
+  gapi.client.sheets.spreadsheets.values.get({
+    spreadsheetId: '1CMfkJYXH5mbUvn9F-SzN4TIxoxn1macPcVKRzxbLfuw',
+    range: 'Plants!A2:AQ',
+  }).then(function(response) {
+    var range = response.result;
+    result = range;
+    plants = range;
+    console.log(range.values.length);
+    processPlants(result);
+    // if (range.values.length > 0) {
+    //   appendPre('Name, Major:');
+    //   for (i = 0; i < range.values.length; i++) {
+    //     var row = range.values[i];
+    //     // Print columns A and E, which correspond to indices 0 and 4.
+    //     appendPre(row[0] + ', ' + row[4]);
+    //   }
+    // } else {
+    //   appendPre('No data found.');
+    // }
+  }, function(response) {
+    appendPre('Error: ' + response.result.error.message);
+  });
+  console.log(result);
+
+}
+console.log(plants);
+
+
+
+
+  /**
+ * Append a pre element to the body containing the given message
+ * as its text node. Used to display the results of the API call.
+ *
+ * @param {string} message Text to be placed in pre element.
+ */
+function appendPre(message) {
+  var pre = document.getElementById('content');
+  var textContent = document.createTextNode(message + '\n');
+  pre.appendChild(textContent);
+}
+
+
+function getPlantList () {
+  gapi.client.sheets.spreadsheets.values.get({
+    spreadsheetId: '16VdfZZNiHPtd9f6MkXjOhXMrvXtTBRZ2wazjracgYkw',
+    range: 'Plants!A1:AQ',
+  }).then(function(response) {
+    var range = response.result;
+   
+    console.log(range.values.length);
+    plants = convertRowToObject(range);
+    getProjectList();
+
+  }, function(response) {
+    appendPre('Error: ' + response.result.error.message);
+  });
+
+}
+function getProjectList () {
+    console.log(plants);
+  gapi.client.sheets.spreadsheets.values.get({
+    spreadsheetId: '1OkEO3P4UpSbDe_-tz5OwKuHNRTk1M277bDAtpmAZZIA',
+    range: 'Projects!A1:T',
+  }).then(function(response) {
+    var range = response.result;
+   
+    console.log(range.values.length);
+    projects = convertRowToObject(range);
+    processData();
+   
+  }, function(response) {
+    appendPre('Error: ' + response.result.error.message);
+  });
+
+}
